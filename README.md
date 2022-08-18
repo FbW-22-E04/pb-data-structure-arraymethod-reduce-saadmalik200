7f927bf21939f0e24d37246f2d00de464e60070b
Saad Malik <saad_malik200@hotmail.com>
2022-08-18 09:26:17 +0200

    ex2

--- README.md
+++ README.md
@@ -1,22 +1,24 @@
 # Array Method .reduce
- Let’s say we received an array of users in the form `[{id:..., name:..., age:... }]`.
 
-* Check out `index.js`, it contains the function `groupById`
-* Complete the function `groupById` so that is creates and returns an object from the passed array.
-* The newly created objects property names should be created from the id of each object in the array id as the key
-* The values of the properties should be the respective objects from the array
+Let’s say we received an array of users in the form `[{id:..., name:..., age:... }]`.
+
+- Check out `index.js`, it contains the function `groupById`
+- Complete the function `groupById` so that is creates and returns an object from the passed array.
+- The newly created objects property names should be created from the id of each object in the array id as the key
+- The values of the properties should be the respective objects from the array
 
 ## Example:
-```js 
+
+```js
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
 let usersById = groupById(users);
 
-console.log(usersById)
+console.log(usersById);
 
 /* expected output:
 {
@@ -25,9 +27,89 @@ console.log(usersById)
   pete: {id: 'pete', name: "Pete Peterson", age: 31},
 }
 */
-``` 
-* Such a function is really handy when working with server data.
+```
 
-* In this task we assume that id is unique. There may be no two array items with the same id.
+- Such a function is really handy when working with server data.
 
-* Please use array .reduce method in the solution.
+- In this task we assume that id is unique. There may be no two array items with the same id.
+
+- Please use array .reduce method in the solution.
+
+/\*\*
+
+- 01
+- Create a function that cleans trailing and leading spaces from an array of products
+-
+- Example:
+- Array [
+  [
+  {
+  name: " D3 5000 iu",
+  description: "Takes care of your immune system",
+  price: 10,
+  },
+  {
+  name: " C 1000mg ",
+  description: "180 tabs of vitamin C with Bioflanoids",
+  price: 3,
+  },
+  {
+  name: " B - Complex 50 mg ",
+  description: "Balanced mix of all basic B vitamins",
+  price: 13,
+  },
+  ]
+-
+- should become [
+-
+- {
+  name: "D3 5000 iu",
+  description: "Takes care of your immune system",
+  price: 10,
+  },
+  {
+  name: "C 1000mg",
+  description: "180 tabs of vitamin C with Bioflanoids",
+  price: 3,
+  },
+  {
+  name: "B - Complex 50 mg",
+  description: "Balanced mix of all basic B vitamins",
+  price: 13,
+  },
+-
+- ]
+-
+- \*/
+
+/\*\*
+
+- 02
+- Create a function to convert miles to kilometers at the end of each property of each item in an arrray
+-
+- Example:
+-
+- [
+- {
+- city: 'Berlin',
+- distance: '639mi'
+- },
+- {
+- city: 'Paris',
+- distance: '1040mi'
+- }
+- ]
+-
+- should become:
+-
+- [
+- {
+- city: 'Berlin',
+- distance: '1028km'
+- },
+- {
+- city: 'Paris',
+- distance: '1673km'
+- }
+- ]
+  \*/
--- index.js
+++ index.js
@@ -1,19 +1,31 @@
-
 //Create a function groupById(arr) that creates an object from it, with id as the key, and array items as values.
 //1
 
 let users = [
-  {id: 'john', name: "John Smith", age: 20},
-  {id: 'ann', name: "Ann Smith", age: 24},
-  {id: 'pete', name: "Pete Peterson", age: 31},
+  { id: "john", name: "John Smith", age: 20 },
+  { id: "ann", name: "Ann Smith", age: 24 },
+
+  { id: "pete", name: "Pete Peterson", age: 31 },
 ];
 
 function groupById(array) {
   //write your code here
+
+  const newobj = array.reduce((accum, item) => {
+    const id = item.id;
+    // console.log(id);
+
+    if (accum[id] === undefined) accum[id] = {};
+    accum[id] = item;
+    // console.log(accum);
+    return accum;
+  }, {});
+  return newobj;
 }
 
-let usersById= groupById(users)
-console.log(usersById)
+let usersById = groupById(users);
+
+// console.log(usersById);
 
 /* expected output: 
 
@@ -22,5 +34,164 @@ console.log(usersById)
   ann: { id: 'ann', name: 'Ann Smith', age: 24 },
   pete: { id: 'pete', name: 'Pete Peterson', age: 31 }
 }
-  
+
+
 */
+
+console.log(`------------------------------------`);
+
+/**
+ * 01
+ * Create a function that cleans trailing and leading spaces from an array of products
+ * 
+ * Example:
+ * Array [
+[
+    {
+      name: "  D3 5000 iu",
+      description: "Takes care of your immune system",
+      price: 10,
+    },
+    {
+      name: "  C 1000mg ",
+      description: "180 tabs of vitamin C with Bioflanoids",
+      price: 3,
+    },
+    {
+      name: " B - Complex 50 mg    ",
+      description: "Balanced mix of all basic B vitamins",
+      price: 13,
+    },
+]
+ * 
+ * should become [
+ * 
+ *  {
+      name: "D3 5000 iu",
+      description: "Takes care of your immune system",
+      price: 10,
+    },
+    {
+      name: "C 1000mg",
+      description: "180 tabs of vitamin C with Bioflanoids",
+      price: 3,
+    },
+    {
+      name: "B - Complex 50 mg",
+      description: "Balanced mix of all basic B vitamins",
+      price: 13,
+    },
+ * 
+ * ]
+ * 
+ * 
+ */
+
+function cleaningTailingSpace(arr) {
+  // console.log(arr);
+
+  const newArr = arr.reduce((accum, item) => {
+    const name = item.name.trim();
+    // console.log(name);
+    // return name;
+    // if (accum[name] === undefined) accum[name] = {};
+    // item[name] = name;
+    // accum[name] = item;
+    item.name = name;
+    // console.log(item);
+    accum.push(item);
+    return accum;
+  }, []);
+  // console.log(newArr);
+}
+
+cleaningTailingSpace([
+  {
+    name: "  D3 5000 iu",
+    description: "Takes care of your immune system",
+    price: 10,
+  },
+  {
+    name: "  C 1000mg ",
+    description: "180 tabs of vitamin C with Bioflanoids",
+    price: 3,
+  },
+  {
+    name: " B - Complex 50 mg    ",
+    description: "Balanced mix of all basic B vitamins",
+    price: 13,
+  },
+]);
+
+// function cleanSpaces(productsArray){
+
+//     productsArray.forEach((element) => {
+//         element.name = element.name.trim();
+//     });
+
+//     return productsArray
+// }
+
+/**
+ * 02
+ * Create a function to convert miles to kilometers at the end of each property of each item in an arrray
+ *
+ * Example:
+ *
+ * [
+ * {
+ *  city: 'Berlin',
+ *  distance: '639mi'
+ * },
+ *  {
+ *  city: 'Paris',
+ *  distance: '1040mi'
+ * }
+ * ]
+ *
+ * should become:
+ *
+ * [
+ *  {
+ *  city: 'Berlin',
+ *  distance: '1028km'
+ * },
+ *  {
+ *  city: 'Paris',
+ *  distance: '1673km'
+ * }
+ * ]
+ */
+
+function mileToKm(array) {
+  array.forEach((item, i) => {
+    // console.log(item);
+    const dist = item.distance.slice(0, -2); //* 1.60934;
+    const newDist = (dist * 1.60934).toFixed() + "km";
+    // console.log(newDist);
+    item.distance = newDist;
+  });
+  console.log(array);
+}
+
+mileToKm([
+  {
+    city: "Berlin",
+    distance: "639mi",
+  },
+  {
+    city: "Paris",
+    distance: "1040mi",
+  },
+]);
+// should become:
+
+// [
+//  {
+//  city: 'Berlin',
+//  distance: '1028km'
+// },
+//  {
+//  city: 'Paris',
+//  distance: '1673km'
+// }
